97a4b111e8ecc24ce193682aecbb621339249523
Ilia Lysov <ilia.lysov.g@gmail.com>
2024-01-28 17:42:52 +0400

    fix: styles

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/shopify-test/",
   plugins: [react()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
 })
